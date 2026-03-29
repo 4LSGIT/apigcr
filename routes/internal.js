a672e6dd44c2702f612f67d561f53934f1f0b3f3
FRED <it@4lsg.com>
2026-03-29 23:54:50 +0300

    added rec to cases

--- routes/internal.js
+++ routes/internal.js
@@ -1,4 +1,5 @@
 // routes/internal.js
+/*
 const express = require("express");
 const router = express.Router();
 
@@ -6,5 +7,23 @@ router.use(require("./internal/sms"));
 router.use(require("./internal/email"));
 router.use(require("./internal/gcal"));
 router.use(require("./internal/sequence"));
+
+module.exports = router;
+*/
+// routes/internal.js
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const router = express.Router();
+const routesPath = path.join(__dirname, "internal");
+
+// Read all files in /routes/internal
+fs.readdirSync(routesPath)
+  .filter(file => file.endsWith(".js"))
+  .forEach(file => {
+    const route = require(path.join(routesPath, file));
+    router.use(route);
+  });
 
 module.exports = router;
