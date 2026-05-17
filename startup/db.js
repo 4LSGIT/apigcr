b35e115c8b810b9d6bfa23a92ccedb3ca5623027
FRED <it@4lsg.com>
2026-05-17 12:40:28 +0300

    dbmanager

--- startup/db.js
+++ startup/db.js
@@ -67,25 +67,3 @@ if (process.env.ENVIRONMENT === "development") {
 }
 
 module.exports = promisePool;
-
-/*const mysql = require("mysql2");
-
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  waitForConnections: true,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
-  timezone: "Z"
-});
-
-pool.on("error", err => {
-  console.error("MySQL pool error:", err);
-});
-
-module.exports = pool.promise();
-*/
