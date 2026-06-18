591c0e4ac8e5eec7a3134c72f5e735fd890b65a7
FRED <it@4lsg.com>
2026-06-18 03:22:58 +0300

    temp route again

--- server.js
+++ server.js
@@ -127,6 +127,7 @@ console.log("db ready");
 // Set port and start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
+  console.log('Node runtime:', process.version);
   console.log(`Server is running on port ${PORT}.`);
   if (process.env.ENVIRONMENT == "development") {
     console.log(`visit http://localhost:${PORT}/`);
